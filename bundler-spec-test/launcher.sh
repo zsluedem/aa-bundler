593ae7835192b461ee0abf9f198229bbca1e9e68
Will Qiu <zsluedem06@gmail.com>
2023-08-16 11:10:11 +0800

    fix: ci entrypoint addr changed

--- bundler-spec-test/launcher.sh
+++ bundler-spec-test/launcher.sh
@@ -15,7 +15,7 @@ case $1 in
         --eth-client-address http://localhost:8545 \
         --mnemonic-file keys/0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 \
         --beneficiary 0x690B9A9E9aa1C9dB991C7721a92d351Db4FaC990 \
-        --entry-points 0x5ff137d4b0fdcd49dca30c7cf57e578a026d2789 \
+        --entry-points 0xd03807FE0a4A2D05ce80D8B2fa85cB317A01eaCd \
         --rpc-api eth,debug,web3 & echo $! > bundler.pid
     popd
 	cd @account-abstraction && yarn deploy --network localhost
